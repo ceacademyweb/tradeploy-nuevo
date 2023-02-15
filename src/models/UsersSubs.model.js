5b5e1c545f3994c29d675beba9a33666296fb4be
Florentino Navarro <ceacademy.web@gmail.com>
2023-02-15 12:55:33 -0600

    api client

--- src/models/UsersSubs.model.js
+++ src/models/UsersSubs.model.js
@@ -1,39 +1,43 @@
 const mongoose = require('mongoose');
 
 const userSubsModel = new mongoose.Schema({
-    name: {
-        type: String,
-    },
-    surname: {
-        type: String,
-    },
-    email: {
-        type: String,
-    },
-    password: {
-        type: String,
-    },
-    telegram: {
-        type: String,
-    },
-    subscribed: {
-        type: Boolean,
-    },
-    subscribedAt: {
-        type: Date,
-    },
-    subscribedUntil: {
-        type: Date,
-    },
-    payId: {
-        type: String,
-    },
-    payObject: {
-        type: Object,
-    },
-    createAt: {
-        type: Date,
-        default: Date.now,
-    }
+  name: {
+    type: String,
+  },
+  surname: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  telegram: {
+    type: String,
+  },
+  subscribed: {
+    type: Boolean,
+  },
+  subscribedAt: {
+    type: Date,
+  },
+  subscribedUntil: {
+    type: Date,
+  },
+  payId: {
+    type: String,
+  },
+  payObject: {
+    type: Object,
+  },
+  isNew: {
+    type: Boolean,
+    default: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 module.exports = mongoose.model('Subscriptions', userSubsModel);
